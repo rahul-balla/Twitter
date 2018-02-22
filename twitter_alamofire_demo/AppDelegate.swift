058842f79adb2b3b27cf8d363230b3fe7fcb3efd
Rahul Balla <rahulballa@pal-nat186-105-87.itap.purdue.edu>
2018-02-21 23:07:22 -0500

    added persisting user feature

--- twitter_alamofire_demo/AppDelegate.swift
+++ twitter_alamofire_demo/AppDelegate.swift
@@ -18,6 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // MARK: TODO: Check for logged in user
         
+        if User.current != nil {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let timelineViewController = storyboard.instantiateViewController(withIdentifier: "TimelineViewController")
+            window?.rootViewController = timelineViewController
+        }
+        
+        else{
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+            window?.rootViewController = loginViewController
+        }
+        
+        
         NotificationCenter.default.addObserver(forName: Notification.Name("didLogout"), object: nil, queue: OperationQueue.main) { (Notification) in
             print("Logout notification received")
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
